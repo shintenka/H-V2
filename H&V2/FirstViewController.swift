109e3b1d2ca6b22fbf5712cb348705a8992d209c
shintenka <tenka.sin@gmail.com>
2017-07-14 22:15:48 +0900

    Initial Commit

---
+++ H&V2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  H&V2
+//
+//  Created by しんてんか on 2017/7/14.
+//  Copyright © 2017年 しんてんか. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
